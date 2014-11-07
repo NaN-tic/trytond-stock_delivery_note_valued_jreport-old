537452400a941da7e72e83c3ff18cc6c06b4bce4
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:44:32 +0100

    Cleanup tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-#This file is part stock_delivery_note_valued_jreport module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part stock_delivery_note_valued_jreport module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from .test_stock_delivery_note_valued_jreport import suite
 
