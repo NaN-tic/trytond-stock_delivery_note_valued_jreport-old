1821571bbc325eac084953af1bbbb8449dc25fa4
resteve <resteve@zikzakmedia.com>
2015-12-01 11:23:56 +0100

    Tests

--- tests/test_stock_delivery_note_valued_jreport.py
+++ tests/test_stock_delivery_note_valued_jreport.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
-# This file is part stock_delivery_note_valued_jreport module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the stock_delivery_note_valued_jreport module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockShipmentoutJreportTestCase(unittest.TestCase):
-    'Test Stock Shipment Out Jreport module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_delivery_note_valued_jreport')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class StockDeliveryNoteValuedJreportTestCase(ModuleTestCase):
+    'Test Stock Delivery Note Valued Jreport module'
+    module = 'stock_delivery_note_valued_jreport'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        StockShipmentoutJreportTestCase))
+        StockDeliveryNoteValuedJreportTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
